5406cc0761e2cff1de482516564e6695c9ed887f
lZackx <lzackx@lzackx.com>
2022-08-03 16:22:14 +0800

    update: CURLTool

--- ZAPM/Classes/Core/ZAPM.h
+++ ZAPM/Classes/Core/ZAPM.h
@@ -8,5 +8,13 @@
 #ifndef ZAPM_h
 #define ZAPM_h
 
+#define ZAPM_DEBUG 1
+
+#if ZAPM_DEBUG
+#define ZLog(s, ...) NSLog(@"[ZAPM]: %@", [NSString stringWithFormat:(s), ##__VA_ARGS__])
+#else
+#define ZLog(...)
+#endif
+
 
 #endif /* ZAPM_h */
